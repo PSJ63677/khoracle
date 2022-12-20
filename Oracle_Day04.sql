9ec8ffadbfb5d438168e7dbed9ce8368772fd58c
PSJ63677 <119032928+PSJ63677@users.noreply.github.com>
2022-12-20 17:48:32 +0900

    FEAT : Oracle day04 Files Complete

--- Oracle_Day04.sql
+++ Oracle_Day04.sql
@@ -4,7 +4,6 @@
 -- LENGTH, LENGTHB(길이구함), SUBSTR(문자자름), INSTR(위치 구함)
 -- LPAD, RPAD
 
-
 -- 2. 숫자처리함수
 -- '982341678934509hello89798739273402'
 SELECT RTRIM(LTRIM('982341678934509hello89798739273402', '0123456789'), '0123456789')
@@ -98,6 +97,24 @@ FROM EMPLOYEE;
 --	ex) 선동일	    1962	    0.3
 --	ex) 송은희	    1963  	    0
 
+-- 날짜 처리 함수
+-- FROM 뒤는 DATE형이 와야하고, TO_DATE뒤는 날짜형식 문자열이와야 한다
+SELECT EXTRACT(YEAR FROM SYSDATE) FROM DUAL;
+SELECT EXTRACT(YEAR FROM HIRE_DATE) FROM EMPLOYEE;
+SELECT EXTRACT(YEAR FROM TO_DATE(EMP_NO)) FROM EMPLOYEE;
+-- ORA-30076: invalid extract field for extract source
+SELECT TO_DATE(EMP_NO) FROM EMPLOYEE;
+-- ORA-01861: literal does not match format string
+-- 660211-1029373 → 661201
+-- YY는 2000년대 RR은 1900년대
+SELECT TO_DATE(SUBSTR(EMP_NO, 1 , 2), 'RR') FROM EMPLOYEE;
+SELECT EXTRACT(YEAR FROM TO_DATE(SUBSTR(EMP_NO, 1 , 2), 'RR')) FROM EMPLOYEE;
+
+-- 문자 처리 함수
+SELECT EMP_NAME AS 직원명, CONCAT(19, SUBSTR(EMP_NO, 1, 2)) AS 년생, NVL(BONUS,0) AS보너스
+FROM EMPLOYEE
+WHERE SUBSTR(EMP_NO, 1, 2) BETWEEN 60 AND 69;
+
 --4. '010' 핸드폰 번호를 쓰지 않는 사람의 수를 출력하시오 (뒤에 단위는 명을 붙이시오)
 --	   인원
 --	ex) 3명
@@ -107,18 +124,31 @@ FROM EMPLOYEE;
 --	    직원명		입사년월
 --	ex) 전형돈		2012년 12월
 --	ex) 전지연		1997년 3월
-
+SELECT EMP_NAME AS 직원명
+, EXTRACT(YEAR FROM HIRE_DATE)||'년 '||EXTRACT(MONTH FROM HIRE_DATE)||'월' 입사년월
+FROM EMPLOYEE
+ORDER BY 2;
 
 --6. 직원명과 주민번호를 조회하시오
 --	단, 주민번호 9번째 자리부터 끝까지는 '*' 문자로 채워서출력 하시오
 --	ex) 홍길동 771120-1******
+SELECT EMP_NAME AS 직원명, SUBSTR(EMP_NO, 1, 8)||'******' AS 주민번호 
+FROM EMPLOYEE;
 
 --7. 직원명, 직급코드, 연봉(원) 조회
 --  단, 연봉은 ￦57,000,000 으로 표시되게 함
 --     연봉은 보너스포인트가 적용된 1년치 급여임
+SELECT EMP_NAME AS 직원명, JOB_CODE AS 직급코드
+, TO_CHAR(SALARY*12+SALARY*BONUS,'L999,999,999,999') AS "연봉(원)", BONUS
+FROM EMPLOYEE;
+-- L Local통화
 
 --8. 부서코드가 D5, D9인 직원들 중에서 2004년도에 입사한 직원중에 조회함.
 --   사번 사원명 부서코드 입사일
+SELECT EMP_ID AS 사번, EMP_NAME AS 사원명, DEPT_CODE AS 부서코드
+, TO_CHAR(HIRE_DATE, 'YYYY-MM-DD') AS 입사일
+FROM EMPLOYEE
+WHERE DEPT_CODE IN('D5', 'D9') AND EXTRACT(YEAR FROM HIRE_DATE) = 2004;
 
 --9. 직원명, 입사일, 오늘까지의 근무일수 조회 
 --	* 주말도 포함 , 소수점 아래는 버림
@@ -129,39 +159,98 @@ FROM EMPLOYEE;
 --   나이는 주민번호에서 추출해서 날짜데이터로 변환한 다음, 계산함
 --	* 주민번호가 이상한 사람들은 제외시키고 진행 하도록(200,201,214 번 제외)
 --	* HINT : NOT IN 사용
+SELECT EMP_NAME AS 직원명, DEPT_CODE AS 부서코드
+, '19'||SUBSTR(EMP_NO, 1, 2)||'년'||SUBSTR(EMP_NO, 3,2)||'월 '||SUBSTR(EMP_NO, 5,2)||'일 ' AS 생년월일
+-- 문자 처리 함수
+, EXTRACT(YEAR FROM TO_DATE(SBSTR(EMP_NO,1,2), 'RR'))
+||'년 '|| EXTRACT(MONTH FROM TO_DATE(SBSTR(EMP_NO,3,2), 'MM'))
+||'월 '|| EXTRACT(DAY FROM TO_DATE(SBSTR(EMP_NO,5,2), 'DD'))
+||'일' AS 생년월일2
+-- 날짜 처리 함수
+, EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM TO_DATE(SBSTR(EMP_NO,1,2), 'RR')) AS "나이(만)"
+-- 날짜 처리 함수
+, EXTRACT(YEAR FROM SYSDATE) - (1900 + TO_NUMBER(SUBSTR(EMP_NO,1,2))) AS 나이
+-- 문자 처리 함수
+, EXTRACT(YEAR FROM SYSDATE) 
+- (DECODE(SUBSTR(EMP_NO, 8, 1), '1', 1900, '2', 1900, 2000)
++ TO_NUMBER(SUBSTR(EMP_NO,1,2))) AS 나이2
+-- DECODE
+FROM EMPLOYEE
+WHERE EMP_ID NOT IN (200, 201, 214);
+
+SELECT 
+    DECODE(SUBSTR(EMP_NO, 8, 1), '1', '남', '2', '여', '3', '남', '4', '여')
+FROM EMPLOYEE;
 
 --11. 사원명과, 부서명을 출력하세요.
 --   부서코드가 D5이면 총무부, D6이면 기획부, D9이면 영업부로 처리하시오.(case 사용)
 --   단, 부서코드가 D5, D6, D9 인 직원의 정보만 조회하고, 부서코드 기준으로 오름차순 정렬함.
-
-
-
+SELECT EMP_NAME AS 사원명
+, DECODE(DEPT_CODE, 'D5', '총무부', 'D6', '기획부', 'D9', '영업부') AS 부서명
+-- CASE 대소비교도 가능
+, CASE
+    WHEN DEPT_CODE = 'D5' THEN '총무부'
+    WHEN DEPT_CODE = 'D6' THEN '기획부'
+    WHEN DEPT_CODE = 'D9' THEN '영업부'
+END AS 부서명
+FROM EMPLOYEE
+WHERE 부서코드 IN('D5', 'D6', 'D9')
+ORDER BY 부서명 ASC;
 
 
 -- 최종 실습 문제
 -- 문제1. 
 -- 입사일이 5년 이상, 10년 이하인 직원의 이름,주민번호,급여,입사일을 검색하여라
+SELECT EMP_NAME AS 이름, EMP_NO AS 주민번호, SARARY AS 급여, HIRE_DATE AS 입사일
+FROM EMPLOYEE
+WHERE CEIL((SYSDATE-HIRE_DATE)/365) BETWEEN 5 AND 10;
 
 -- 문제2.
 -- 재직중이 아닌 직원의 이름,부서코드, 고용일, 근무기간, 퇴직일을 검색하여라 
 --(퇴사 여부 : ENT_YN)
+SELECT EMP_NAME AS 이름, DEPT_CODE AS 부서코드, HIRE_DATE AS 입사일
+, (ENT_DATE-HIREDATE) AS 근무기간,  ENT_DATE AS 퇴사일
+FROM EMPLOYEE
+-- WHERE ENT_YN != 'N';
+WHERE ENT_YP = 'Y';
 
 -- 문제3.
 -- 근속년수가 10년 이상인 직원들을 검색하여
 -- 출력 결과는 이름,급여,근속년수(소수점X)를 근속년수가 오름차순으로 정렬하여 출력하여라
 -- 단, 급여는 50% 인상된 급여로 출력되도록 하여라.
+SELECT EMP_NAME AS 이름, SALARY AS 급여, CEIL((SYSDATE-HIRE_DATE)/365) AS 근속년수
+FROM EMPLOYEE
+WHERE CEIL((SYSDATE-HIRE_DATE)/365) >= 10
+ORDER BY 3 ASC;
 
 -- 문제4.
 -- 입사일이 99/01/01 ~ 10/01/01 인 사람 중에서 급여가 2000000 원 이하인 사람의
 -- 이름,주민번호,이메일,폰번호,급여를 검색 하시오
+SELECT EMP_NAME AS 이름, EMP_NO AS 주민번호, EMAIL AS 이메일,PHONE AS 폰번호, SALARY AS 급여
+FROM EMPLOYEE
+WHERE (HIRE_DATE BETWEEN TO_DATE('99/01/01') AND TO_DATE('10/01/01')) AND SALARY <= 2000000;
 
 -- 문제5.
 -- 급여가 2000000원 ~ 3000000원 인 여직원 중에서 4월 생일자를 검색하여 
 -- 이름,주민번호,급여,부서코드를 주민번호 순으로(내림차순) 출력하여라
 -- 단, 부서코드가 null인 사람은 부서코드가 '없음' 으로 출력 하여라.
+-- 이름,주민번호,이메일,폰번호,급여를 검색 하시오
+SELECT NVL(DEPT_CODE,'없음') FROM EMPLOYEE
+WHERE DEPT_CODE IS NULL;
+SELECT EMP_NAME AS 이름, EMP_NO AS 주민번호, EMAIL AS 이메일,PHONE AS 폰번호, SARARY AS 급여
+FROM EMPLOYEE
+WHERE (SALARY BETWEEN 2000000 AND 3000000) 
+-- AND SUBSTR(EMP_NO, 8, 1) = '2' AND SUBSTR(EMP_NO, 3, 2) = '04'
+AND EMP_NO LIKE '__04__-2%'
+ORDER BY 주민번호 DESC;
 
 -- 문제6.
 -- 남자 사원 중 보너스가 없는 사원의 오늘까지 근무일을 측정하여 
 -- 1000일 마다(소수점 제외) 
 -- 급여의 10% 보너스를 계산하여 이름,특별 보너스 (계산 금액) 결과를 출력하여라.
 -- 단, 이름 순으로 오름 차순 정렬하여 출력하여라.
+SELECT EMP_NAME AS 이름, ((SYSDATE-HIRE_DATE)/1000)*0.1*SALARY AS "특별 보너스"
+FROM EMPLOYEE
+-- WHERE SUBSTR(EMP_NO, 8, 1) = 1 AND BONUS IS NULL
+WHERE EMP_NO LIKE '%-1%' AND BONUS IS NULL
+ORDER BY 이름 ASC;
